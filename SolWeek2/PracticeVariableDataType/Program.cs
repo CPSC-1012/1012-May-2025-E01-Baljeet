21573892c444cc699a91b0297c14243d1e0d49f2
BaljeetK79 <baljeetk@nait.ca>
2025-05-16 19:25:47 -0600

    Practice Variables and Data Types

---
+++ SolWeek2/PracticeVariableDataType/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticeVariableDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
