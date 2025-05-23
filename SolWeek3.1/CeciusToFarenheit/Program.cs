3503663d7dfd4ef9a0498cbb06b11e4cf596d2d5
Baljeet Kaur <BALJEETK@nait.ca>
2025-05-23 17:56:58 -0600

    Celsius To Farenheit

---
+++ SolWeek3.1/CeciusToFarenheit/Program.cs
@@ -0,0 +1,35 @@
+﻿/*  * This is a simple C# program that converts Celsius to Fahrenheit.
+ * It takes a temperature in Celsius as input and
+ *  outputs the equivalent temperature in Fahrenheit.
+ * Author: Baljeet Kaur
+ * Last updated: May 23, 2025
+ */
+
+
+namespace CeciusToFarenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarations
+
+            double degreeCelsius, degreeFahrenheit;
+
+            //Input
+            Console.Write("Enter the temperature in Celsius: ");
+            degreeCelsius = double.Parse(Console.ReadLine());
+
+            //Conversion formula
+
+            degreeFahrenheit = (degreeCelsius * 9 / 5) + 32;
+
+            //Output
+
+            Console.WriteLine(" Celsius is equal to :"+ degreeFahrenheit + "F");
+
+
+
+        }
+    }
+}
