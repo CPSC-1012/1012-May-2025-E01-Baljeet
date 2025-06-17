bf57f3dab6bf00d07c077caf54de0922b6690f9d
Baljeet Kaur <BALJEETK@nait.ca>
2025-06-16 18:26:50 -0600

    Example Exception Handling

---
+++ SolWeek7/PracticeExceptionHandling/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace PracticeExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int i = 5, j = 0;
+            // error handling by code
+            /*
+            if (j != 0)
+            {
+                Console.WriteLine($" Result of {i} Divide by {j} is : {i / (double) j}");
+            }
+            else
+            {
+                Console.WriteLine("Can't Divide by Zero");
+            }
+            */
+
+            // error handling by try catch block
+            /*
+            try
+            {
+                Console.WriteLine($" Result of {i} Divide by {j} is : {i / j}");
+            }
+
+            catch
+            {
+                Console.WriteLine("Can't Divide by Zero");
+            }
+            */
+            // catch block for specific exception handling
+
+            try
+            {
+                Console.WriteLine($" Result of {i} Divide by {j} is : {i / j}");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+        }
+    }
+}
