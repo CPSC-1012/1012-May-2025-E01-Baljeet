b71f56a3a15f9011d674610fe40a70e04b341589
Baljeet Kaur <BALJEETK@nait.ca>
2025-06-23 18:15:46 -0600

    Claculator using Methods

--- SolWeek7.1/PracticeArray/Program.cs
+++ SolWeek7.1/PracticeArray/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-
+            Console.Clear();
             // array of string : days of week
             // array of double: marks of student
             /*
@@ -22,22 +22,41 @@
             {
                 someNumbers[index] = keygen.Next(1, 11);
             }
-            */
-            const int ARRAY_SIZE = 10;
+            
+            const int ARRAY_SIZE = 5;
 
-            string[] studentNames = new string[ARRAY_SIZE] { "Shane", "Alex", "Adam", "Pamela", "Catherine", "Hushly", "Remy", "Jerry", "A","b" }; ;
+            string[] studentNames = new string[ARRAY_SIZE];// { "Shane", "Alex", "Adam", "Pamela", "Catherine" }; 
             int[] studentID = new int[ARRAY_SIZE];
 
             for (int i = 0; i <= (ARRAY_SIZE -1) ; i++)
             {
-                Console.WriteLine($"Enter student ID {i} : ");
+                Console.Write($"Enter student ID {i} : ");
                 studentID[i] = int.Parse(Console.ReadLine());
-                Console.WriteLine($" Enter student Name {i} ;");
+                Console.Write($" Enter student Name {i} ; ");
                 studentNames[i] = Console.ReadLine();
+                Console.WriteLine();
+
             }
 
-           
-  
+            /// array loop for printing the elements 
+            /// 
+            for (int j = 0; j < studentNames.Length; j++)
+            {
+                Console.WriteLine($" Student ID is {studentID[j]}, and the student name is : {studentNames[j]}");
+            }
+
+            */
+
+            string[] monthNames = new string[] { null, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            int monthNumber;
+            Console.WriteLine(monthNames.Length);
+
+            Console.Write("Enter the number of the Month");
+            monthNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"For Month number {monthNumber} name is : {monthNames[monthNumber]}");
+
+
         }
     }
 }
---
+++ SolWeek8/PracticeMethods/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+
+namespace PracticeMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            ///Example : Calculator Methods 
+            ///
+            double a , b, result=0;
+            int choice;
+            char option;
+
+            //input
+            do
+            {
+                Console.Write(" Enter two values for operation : ");
+                a = double.Parse(Console.ReadLine());
+                b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine(" Press 1 for addition");
+                Console.WriteLine(" Press 2 for subtraction");
+                Console.WriteLine(" Press 3 for Multiplication");
+                Console.WriteLine(" Press 4 for Division");
+
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        result = Addition(a, b);
+                        break;
+                    case 2:
+                        result = Subtraction(a, b);
+                        break;
+                    case 3:
+                        result = Multiplication(a, b);
+                        break;
+                    case 4:
+                        result = Division(a, b);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter value between 1 - 4 ");
+                        break;
+
+                }
+
+                Console.WriteLine($" For operation {choice} on input values {a} , {b} the result is : {result}");
+
+                Console.WriteLine("do you want to continue: Y or N ");
+                option = char.Parse(Console.ReadLine());
+
+            }while ( option == 'Y' || option == 'y');
+
+            double Addition (double x, double y)
+            { 
+                return (x + y);
+            }
+
+            double Subtraction(double x, double y)
+            {
+                return (x - y);
+
+            }
+
+            double Multiplication(double x, double y)
+            {
+                return (x * y);
+            }
+
+            double Division(double x, double y)
+            {
+                return (x / y);
+            }
+        }
+    }
+}
