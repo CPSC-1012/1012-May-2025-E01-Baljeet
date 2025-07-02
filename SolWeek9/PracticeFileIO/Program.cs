d75874094ff5f4f90edaf23770724e948ea7e92c
Baljeet Kaur <BALJEETK@nait.ca>
2025-07-02 17:29:32 -0600

    Wiriting into a file

---
+++ SolWeek9/PracticeFileIO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticeFileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // Program to read and write from a file 
+
+            // start with writing some content to a file
+
+            StreamWriter fileWriter = new StreamWriter("cpscEO1.txt");
+            fileWriter.WriteLine("This is first attempt to write in a file");
+            fileWriter.Close();
+
+        }
+    }
+}
