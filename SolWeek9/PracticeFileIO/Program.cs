5839945eb9da7f8110409059eb6e3b89bd4e71da
Baljeet Kaur <BALJEETK@nait.ca>
2025-07-02 17:46:55 -0600

    Writing in two different files in different drives.

--- SolWeek9/PracticeFileIO/Program.cs
+++ SolWeek9/PracticeFileIO/Program.cs
@@ -10,9 +10,13 @@
 
             // start with writing some content to a file
 
-            StreamWriter fileWriter = new StreamWriter("cpscEO1.txt");
-            fileWriter.WriteLine("This is first attempt to write in a file");
-            fileWriter.Close();
+            StreamWriter fileWriter = new StreamWriter("D://NAIT/cpscEO1.txt");
+            fileWriter.WriteLine("This is my new attempt to write in a file");
+
+            StreamWriter fileWriter2 = new StreamWriter("E://NAIT/cpscEO2.txt");
+            fileWriter2.WriteLine("This is second attempt to write in a file");
+           fileWriter.Close();
+            fileWriter2.Close();
 
         }
     }
--- SolWeek9/PracticeFileIO/obj/Debug/net8.0/PracticeFileIO.AssemblyInfo.cs
+++ SolWeek9/PracticeFileIO/obj/Debug/net8.0/PracticeFileIO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeFileIO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735bfe2463d9cb4b83d805586a11b414e8020b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75874094ff5f4f90edaf23770724e948ea7e92c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticeFileIO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticeFileIO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
