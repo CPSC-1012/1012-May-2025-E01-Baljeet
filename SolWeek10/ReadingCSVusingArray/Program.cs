9ef68ce3464a50b242365a9f54a8d1be9536fe1d
Baljeet Kaur <BALJEETK@nait.ca>
2025-07-07 18:26:20 -0600

    Display CSV using Array

---
+++ SolWeek10/ReadingCSVusingArray/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ReadingCSVusingArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading from a CSV using Array");
+
+            //declaration
+
+            string fileName;
+            int recordCount = 0;
+            string record;
+
+
+
+            //input
+
+            Console.Write(" Enter name of file");
+            fileName = Console.ReadLine();
+
+            // open file for reading
+
+            StreamReader reader = new StreamReader(fileName);
+
+            while (reader.EndOfStream == false) // read till the pointer reaches to end of file
+            {
+
+                recordCount++;
+                record = reader.ReadLine();
+
+                Console.WriteLine($" Record : {recordCount}");
+                Console.WriteLine("--------------------" );
+
+                string[] fields = record.Split(','); // splits string into string of array
+
+                for (int idx = 0;  idx < fields.Length ; idx++)
+                {
+                    Console.WriteLine($" Field {idx+1} : {fields[idx]}");
+                }
+
+            }
+
+
+        }
+    }
+}
