85dea5818b216b852e02c480b48e60fbd4ca8f4c
Baljeet Kaur <BALJEETK@nait.ca>
2025-07-28 18:20:27 -0600

    Sample code for classes and array of objects

---
+++ SolWeek13/PracticeClasses/PhReading.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHReading_Class_Example
+{
+    internal class PhReading
+    {
+        private double _pH;
+        private string _date;
+
+        /// <summary>
+        /// Creates a new pH reading
+        /// </summary>
+        /// <param name="ph">The actual pH value recorded</param>
+        /// <param name="date">The date the pH value was recorded</param>
+        public PhReading(double ph, string date)
+        {
+            Ph = ph;
+            Date = date;
+        }
+
+        /// <summary>
+        /// Ph property used to set and get the internal pH
+        /// </summary>
+        public double Ph
+        {
+            get
+            {
+                return _pH;
+            }
+
+            set
+            {
+                if (value < 0 || value > 14)
+                {
+                    throw new Exception("ph must be between 0 and 14");
+                }
+
+                _pH = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Sets and gets the date internal value
+        /// </summary>
+        public string Date
+        {
+            get
+            {
+                return _date;
+            }
+
+            set
+            {
+                if (IsValidDateFormat(value) == false)
+                {
+                    throw new Exception("Date must be in the format MM-DD-YYYY");
+                }
+
+                _date = value;
+            }
+        }
+
+        /// <summary>
+        /// Validates a date. Must be in the format MM-DD-YYYY.
+        /// </summary>
+        /// <param name="date">The date to check</param>
+        /// <returns>True if the date is valid, false otherwise.</returns>
+        public static bool IsValidDateFormat(string date)
+        {
+            bool isValid = true;
+            string[] parts = date.Split('-');
+            int day;
+            int month;
+            int year;
+
+            try
+            {
+                if (parts.Length != 3)
+                {
+                    isValid = false;
+                }
+                else if (parts[0].Length != 2 || parts[1].Length != 2 || parts[2].Length != 4)
+                {
+                    isValid = false;
+                }
+                else
+                {
+                    // check that all parts are ints
+                    month = int.Parse(parts[0]);
+                    day = int.Parse(parts[1]);
+                    year = int.Parse(parts[2]);
+
+                    // check for correct ranges
+                    if (month < 1 || month > 12)
+                    {
+                        isValid = false;
+                    }
+                    else if (day < 1 || day > 31)
+                    {
+                        isValid = false;
+                    }
+                    else if (year < 1000 || year > 9999)
+                    {
+                        isValid = false;
+                    }
+                }
+            }
+            catch
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ SolWeek13/PracticeClasses/Program.cs
@@ -0,0 +1,110 @@
+﻿using PHReading_Class_Example;
+
+namespace PracticeClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Individual variables
+            /*
+            Console.Write("Enter PH value");
+            double PH = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Date value");
+            string date = Console.ReadLine();
+
+            PhReading reading1 = new PhReading(PH, date);
+            Console.WriteLine($"Reading for {reading1.Date} is {reading1.Ph:F1}");
+            */
+            // Using an array
+            
+            const int MAX_SIZE = 31;
+            PhReading[] readings = new PhReading[MAX_SIZE];
+            int count = 0; // count of the readings entered
+            string again = "y";
+
+            while (again == "y" && count < MAX_SIZE)
+            {
+                Console.Write($"Enter the date for the reading {count + 1} (MM-DD-YYYY): ");
+                string date = Console.ReadLine();
+
+                // TODO: exception handling
+                Console.Write($"Enter the pH for the reading {count + 1}: ");
+                double pH = double.Parse(Console.ReadLine());
+
+                if (PhReading.IsValidDateFormat(date) && pH >= 0 && pH <= 14)
+                {
+                    readings[count] = new PhReading(pH, date);
+                    count += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, date or pH value were invalid.");
+                }
+
+                Console.Write("Enter another (y/n): ");
+                again = Console.ReadLine();
+            }
+
+            // Display the entries
+            for (int i = 0; i < count; i += 1)
+            {
+                Console.WriteLine($"Reading for {readings[i].Date} is {readings[i].Ph:F1}");
+            }
+
+            
+
+
+            /*
+             * 
+             * / Individual variables
+//PhReading reading1 = new PhReading(7.0, "07-27-2023");
+//Console.WriteLine($"Reading for {reading1.Date} is {reading1.Ph:F1}");
+
+// Using an array
+const int MAX_SIZE = 31;
+PhReading[] readings = new PhReading[MAX_SIZE];
+int count = 0; // count of the readings entered
+string again = "y";
+
+while (again == "y" && count < MAX_SIZE)
+{
+    Console.Write($"Enter the date for the reading {count + 1} (MM-DD-YYYY): ");
+    string date = Console.ReadLine();
+
+    // TODO: exception handling
+    Console.Write($"Enter the pH for the reading {count + 1}: ");
+    double pH = double.Parse(Console.ReadLine());
+
+    if (PhReading.IsValidDateFormat(date) && pH >= 0 && pH <= 14)
+    {
+        readings[count] = new PhReading(pH, date);
+        count += 1;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, date or pH value were invalid.");
+    }
+
+    Console.Write("Enter another (y/n): ");
+    again = Console.ReadLine();
+}
+
+// Display the entries
+for (int i = 0; i < count; i += 1)
+{
+    Console.WriteLine($"Reading for {readings[i].Date} is {readings[i].Ph:F1}");
+}
+
+             * 
+             * 
+             */
+
+
+
+
+        }
+    }
+}
